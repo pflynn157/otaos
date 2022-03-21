e60c0e818bc5d89cdde36fca9a4a45aba2a34929
Patrick Flynn <patrickf4664@protonmail.com>
2022-03-21 09:54:35 -0400

    Reworked file system driver to work via bytes

--- kernel/fs.c
+++ kernel/fs.c
@@ -38,20 +38,31 @@ void fs_init() {
 
 // Opens a file on the disk
 int fs_open(const char *path) {
-    uint16_t data[256];
+    uint16_t data1[256];
     int root_count = 0;
     int index = 0;
     for (size_t i = fs_sector_start; i<100; i++) {
-        memset(data, 0, 256);
-        ata_read(0, i, data);
+        memset(data1, 0, 256);
+        ata_read(0, i, data1);
         
         size_t j = 0;
         for (j = 0; j<256; j++) {
-            if (data[j] == 0x0FAB) break;
+            if (data1[j] == 0x0FAB) break;
         }
-        root_count = data[j+1];
+        
+        root_count = data1[j+1];
         index = j + 2;
         break;
+    }
+    
+    // Now, break everything up into bytes
+    index *= 2;
+    uint8_t data[512];
+    for (int i = 0, k = 0; i<256; i++, k+=2) {
+        uint8_t c1 = data1[i] >> 8;
+        uint8_t c2 = data1[i];
+        data[k] = c2;
+        data[k+1] = c1;
     }
     
     // At this point, we're in the root directory and we know
@@ -59,31 +70,25 @@ int fs_open(const char *path) {
     //
     // Now, start reading files
     for (int i = 0; i<root_count; i++) {
-        uint16_t next = data[index];
-        ++index;
+        uint16_t next = ((uint16_t)data[index+1] << 8) | data[index];
+        index += 2;
         
         // This first element should contain the type
         if (next != 0x0010) {
             // TODO: Something...
         }
         
-        uint16_t content_size = data[index];
-        uint16_t name_size = data[index+1];
+        uint16_t content_size = ((uint16_t)data[index+1] << 8) | data[index];
+        index += 2;
+        uint16_t name_size = ((uint16_t)data[index+1] << 8) | data[index];
         index += 2;
         
         char name[name_size];
         int name_index = 0;
-        
-        // Now, read the name
-        for (int j = 0; j<name_size/2; j++) {
-            uint16_t current = data[index];
+        for (int j = 0; j<name_size; j++) {
+            name[name_index] = data[index];
+            ++name_index;
             ++index;
-            
-            char c1 = current;
-            char c2 = current >> 8;
-            name[name_index] = c1;
-            name[name_index+1] = c2;
-            name_index += 2;
         }
         name[name_index] = 0;
         
@@ -101,15 +106,9 @@ int fs_open(const char *path) {
             // Break up the current chunk into something readable,
             // and then set the filesystem state variables
             //
-            for (int j = 0, k = 0; j<256; j++, k += 2) {
-                uint16_t current = data[j];
-                char c1 = current;
-                char c2 = current >> 8;
-                fs_current_data[k] = c1;
-                fs_current_data[k+1] = c2;
-            }
+            for (int j = 0; j<512; j++) fs_current_data[j] = data[j];
             fs_current_sector = i;
-            fs_current_chunk = index * 2;
+            fs_current_chunk = index;
             return 1;
         }
     }
